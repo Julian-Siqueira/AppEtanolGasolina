6e3c3597a35c42733d5cfc8ca01773206c5ba4ca
Dev-Julian-Siqueira <juliansiqueiradev@outlook.com>
2024-07-12 20:02:25 -0300

    Criação da inicializacao dos componentes.

--- app/src/main/java/devandroid/julian/appetanolgasolina/MainActivity.kt
+++ app/src/main/java/devandroid/julian/appetanolgasolina/MainActivity.kt
@@ -1,12 +1,32 @@
 package devandroid.julian.appetanolgasolina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    /*
+    criaçao de variaveis que vao ser utilizadas posteriormente
+    private, pois elas so vao ser acessadas pela MainActivity
+    lateinit var, pois quero que elas inicializem quando a tela for criada
+    */
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editAlcool: TextInputEditText
+
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editGasolina: TextInputEditText
+
+    private lateinit var btnCalcular: Button
+
+    private lateinit var textResultado: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //criar o método inicializacaoDeComponentes
+        inicializarComponentesInterface()
+    }
+
+    //crio a funçao do método
+    private fun inicializarComponentesInterface() {
+
+
+        //aqui eu LINKO as variaveis que criei como lateinit, com os ids dos componentes do layout.
+
+
+        textInputAlcool = findViewById(R.id.text_input_alcool)
+        editAlcool = findViewById(R.id.edit_text_alcool)
+
+        textInputGasolina = findViewById(R.id.text_input_gasolina)
+        editGasolina = findViewById(R.id.edit_text_gasolina)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        textResultado = findViewById(R.id.text_view_resultado)
     }
 }
